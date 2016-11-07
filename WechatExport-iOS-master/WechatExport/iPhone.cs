0b24f0ddd5eb2122558ff9458012f763ea98a1f6
Jackpan0514 <pan800215@qq.com>
2016-11-08 04:41:08 +0800

    ok
    ww

---
+++ WechatExport-iOS-master/WechatExport/iPhone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace iphonebackupbrowser
+{
+
+    //
+    // backup information retrieved from Info.plist
+    //
+    class iPhoneBackup
+    {
+        public string DeviceName;
+        public string DisplayName;
+        public DateTime LastBackupDate;     // originally a string
+
+        public string path;                 // backup path
+
+        public override string ToString()
+        {
+            string str = DisplayName + " (" + LastBackupDate + ")";
+            if (custom) str = str + " *";
+            return str;
+        }
+
+        public bool custom = false;         // backup loaded from a custom directory
+        public int index;                   // index in the combobox control
+
+        // delegate to sort backups (newer first)
+        public static int SortByDate(iPhoneBackup a, iPhoneBackup b)
+        {
+            return b.LastBackupDate.CompareTo(a.LastBackupDate);
+        }
+    }
+}
